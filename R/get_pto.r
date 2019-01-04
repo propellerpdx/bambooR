61c1b2416fc2923c40ba6180baffb8ff465e1f94
Mark Druffel <mark.druffel@gmail.com>
2019-01-04 08:38:26 -0800

    Update to column names for get_pto.r

--- R/get_pto.r
+++ R/get_pto.r
@@ -52,7 +52,7 @@ get_pto <- function(user=NULL,password=NULL){
   time_off <- dplyr::inner_join(time_off,time_off_days) %>%
     dplyr::mutate(Spent_Date = lubridate::ymd(Date),
                   Updated_Date = lubridate::ymd(lastChanged)) %>%
-    dplyr::rename('Bamboo_ID'='id','Employee_ID'='employeeId','Status'='status','PTO_Type'='name','Hours_Unit'='unit','Total_Amount'='amount') %>%
-    dplyr::select(Bamboo_ID,Employee_ID,Spent_Date,Updated_Date,Status,PTO_Type,Total_Amount,Hours)
+    dplyr::rename('Entry_ID'='id','Bamboo_ID'='employeeId','Status'='status','PTO_Type'='name','Hours_Unit'='unit','Total_Amount'='amount') %>%
+    dplyr::select(Entry_ID,Bamboo_ID,Spent_Date,Updated_Date,Status,PTO_Type,Total_Amount,Hours)
   return(time_off)
 }
